c584779978c4dd26550ce1540095b976df913405
Ayat <ayatbaeshev@gmail.com>
2023-08-19 01:24:47 +0200

    changed http request for https

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,6 +1,6 @@
 export async function getWeather(location){
     try{
-        let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=cc2785c69424465cbda213523231808&q=${location}`);
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=cc2785c69424465cbda213523231808&q=${location}`);
         let weather =  await response.json();
         // console.table(weather)
         console.log(weather);
